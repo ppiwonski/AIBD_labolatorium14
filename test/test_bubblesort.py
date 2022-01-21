226724bbf17f56169fd234eeaf4eb1ba3c5289f6
ppiwonski <61936095+ppiwonski@users.noreply.github.com>
2022-01-21 10:26:35 +0100

    Zakończone zadanie - komentarze

--- test/test_bubblesort.py
+++ test/test_bubblesort.py
@@ -1,19 +1,28 @@
 import pytest
 from bubblesort import bubble
 
-testdata=[[1,5,2,5,3,8]]
+#przypadki testowe
+testdata=[([1,5,2,5,3,8],[1,2,3,5,5,8]),
+            ([1,1,1,1],[1,1,1,1]),
+            ([5,6,-2,-3],[-3,-2,5,6])]
 testdata1=[[]]
-@pytest.mark.parametrize('sample', testdata)
+testdata2=[[1,5,2,5,3,'a']]
 
-def test_bubble(sample):
-    got=bubble(sample)
-    expected_output=[1,2,3,5,5,8]
-    assert got == expected_output
+#sprawdzenie poprawności wyników
+@pytest.mark.parametrize('sample,expected_output', testdata)
+def test_bubble(sample,expected_output):
+    assert bubble(sample) == expected_output
 
-
+#sprawdzenie pustej listy
 @pytest.mark.parametrize('sample', testdata1)
-
 def test_bubble1(sample):
     got=bubble(sample)
     expected_output=[]
+    assert got == expected_output
+
+#sprawdzenie typów danych
+@pytest.mark.parametrize('sample', testdata2)
+def test_bubble2(sample):
+    got=bubble(sample)
+    expected_output=None
     assert got == expected_output
